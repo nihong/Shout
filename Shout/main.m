a7f39020ad1640a9bd8e8b3ea6e156ad8d51215c
Roy Gao <1241683303@qq.com>
2017-11-29 22:51:05 +0800

    Initial Commit

---
+++ Shout/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Shout
+//
+//  Created by 泥红 on 2017/11/29.
+//  Copyright © 2017年 RoyGao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
